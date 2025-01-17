bb4fcf1dfc6a0733ade9071368586d4a54ea222b
Aureo Dantas <dantasaureo19@gmail.com>
2025-01-17 02:24:45 -0300

    insights

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,13 @@
 import { Analytics } from "@vercel/analytics/react"
 import { StrictMode } from 'react';
+import { SpeedInsights } from "@vercel/speed-insights/next"
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <StrictMode>
+    <SpeedInsights />
     <Analytics />
     <App />
 
