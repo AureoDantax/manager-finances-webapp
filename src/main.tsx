728b339e8487efac68a32ad89ce774d50628fe7f
Aureo Dantas <dantasaureo19@gmail.com>
2025-01-17 03:38:12 -0300

    remove imports

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,12 @@
-import { Analytics } from "@vercel/analytics/react"
+import { Analytics } from "@vercel/analytics/react";
 import { StrictMode } from 'react';
-import { SpeedInsights } from "@vercel/speed-insights/next"
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <StrictMode>
-    
     <Analytics />
     <App />
-
   </StrictMode>
 );
